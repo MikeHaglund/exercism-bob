6bb3e80be484c65cc10921a842e3b7804287a4d8
Mike Haglund <44382034+MikeHaglund@users.noreply.github.com>
2018-11-13 16:26:29 -0700

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const hey = (message) => {
+  message = message.trim()
+  const question = message.endsWith('?')
+  const yelling = message.toUpperCase() === message
+  const empty = message === ''
+  const letters = message.match(/[a-z]/i)
+  let result;
+
+  if (empty)
+    result = 'Fine. Be that way!'
+  else if (yelling && question && letters)
+    result = 'Calm down, I know what I\'m doing!'
+  else if (question)
+    result = 'Sure.' 
+  else if (yelling && letters)
+    result = 'Whoa, chill out!'
+  else 
+    result = 'Whatever.'
+    
+  return result
+}
